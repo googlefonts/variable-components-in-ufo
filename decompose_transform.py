2e317a746b7f08347fbd7f664c91f98b64808ae8
Just van Rossum <justvanrossum@gmail.com>
2022-09-08 19:26:12 +0200

    Add code to demonstrate composition/decomposition

---
+++ decompose_transform.py
@@ -0,0 +1,62 @@
+from typing import NamedTuple
+import math
+from fontTools.misc.transform import Transform
+
+
+class DecomposedTransform(NamedTuple):
+    x: float
+    y: float
+    rotationAngle: float
+    scaleX: float
+    scaleY: float
+    skewAngleX: float
+    skewAngleY: float
+
+
+def composeTransform(
+    x: float,
+    y: float,
+    rotationAngle: float,
+    scaleX: float,
+    scaleY: float,
+    skewAngleX: float,
+    skewAngleY: float,
+) -> Transform:
+    """Compose a deocomposed transform into an Affine transform."""
+    t = Transform()
+    t = t.translate(x, y)
+    t = t.rotate(rotationAngle)
+    t = t.scale(scaleX, scaleY)
+    t = t.skew(skewAngleX, skewAngleY)
+    return t
+
+
+def decomposeTransform(transform: Transform) -> DecomposedTransform:
+    """Decompose an Affine transformation matrix into components."""
+    a, b, c, d, x, y = transform
+    delta = a * d - b * c
+
+    rotationAngle = 0
+    scaleX = scaleY = 0
+    skewAngleX = skewAngleY = 0
+
+    # Apply the QR-like decomposition.
+    if a != 0 or b != 0:
+        r = math.sqrt(a * a + b * b)
+        rotationAngle = math.acos(a / r) if b > 0 else -math.acos(a / r)
+        scaleX, scaleY = (r, delta / r)
+        skewAngleX, skewAngleY = (math.atan((a * c + b * d) / (r * r)), 0)
+    elif c != 0 or d != 0:
+        s = math.sqrt(c * c + d * d)
+        rotationAngle = math.pi / 2 - (
+            math.acos(-c / s) if d > 0 else -math.acos(c / s)
+        )
+        scaleX, scaleY = (delta / s, s)
+        skewAngleX, skewAngleY = (0, math.atan((a * c + b * d) / (s * s)))
+    else:
+        # a = b = c = d = 0
+        pass
+
+    return DecomposedTransform(
+        x, y, rotationAngle, scaleX, scaleY, skewAngleX, skewAngleY
+    )
---
+++ decompose_transform_test.py
@@ -0,0 +1,103 @@
+import math
+from fontTools.misc.transform import Transform
+import pytest
+from decompose_transform import (
+    composeTransform,
+    decomposeTransform,
+    DecomposedTransform,
+)
+
+
+def transformEqual(t1, t2):
+    return all(math.isclose(v1, v2, abs_tol=1e-09) for v1, v2 in zip(t1, t2))
+
+
+def decomposedTransformEqual(t1, t2):
+    return all(
+        math.isclose(v1, v2, abs_tol=1e-09)
+        for v1, v2 in zip(t1._asdict().values(), t2._asdict().values())
+    )
+
+
+test_data = [
+    (
+        DecomposedTransform(0, 0, 0, 1, 1, 0, 0),
+        Transform(1, 0, 0, 1, 0, 0),
+    ),
+    (
+        DecomposedTransform(0, 0, -math.pi, 1, 1, 0, 0),
+        Transform(-1, 0, 0, -1, 0, 0),
+    ),
+    (
+        DecomposedTransform(0, 0, math.pi / 2, 1, 1, 0, 0),
+        Transform(0, 1, -1, 0, 0, 0),
+    ),
+    (
+        DecomposedTransform(0, 0, 0, 1, 1, math.pi / 4, 0),
+        Transform(1, 0, 1, 1, 0, 0),
+    ),
+    (
+        DecomposedTransform(0, 0, 0, 1, 1, 0, math.pi / 4),
+        Transform(1, 1, 0, 1, 0, 0),
+    ),
+    (
+        DecomposedTransform(0, 0, math.pi / 4, 1, 1, 0, 0),
+        Transform(
+            0.7071067811865476,
+            0.7071067811865475,
+            -0.7071067811865475,
+            0.7071067811865476,
+            0.0,
+            0.0,
+        ),
+    ),
+    (
+        DecomposedTransform(0, 0, math.pi / 4, 2, 1, 0, 0),
+        Transform(
+            1.4142135623730951,
+            1.414213562373095,
+            -0.7071067811865475,
+            0.7071067811865476,
+            0.0,
+            0.0,
+        ),
+    ),
+    (
+        DecomposedTransform(100, 150, 0.1, 4, 3, 0.5, 0),
+        Transform(
+            3.980016661112103,
+            0.3993336665873126,
+            1.874792761644827,
+            3.203169472169176,
+            100.0,
+            150.0,
+        ),
+    ),
+]
+
+
+@pytest.mark.parametrize("decomposed, composed", test_data)
+def test_composeTransform(decomposed, composed):
+    print(decomposed)
+    t = composeTransform(**decomposed._asdict())
+    assert transformEqual(composed, t), tuple(t)
+
+
+@pytest.mark.parametrize("decomposed, composed", test_data)
+def test_decomposeTransform(decomposed, composed):
+    dec = decomposeTransform(composed)
+    if decomposed.skewAngleY:
+        # decomposition is ambiguous, and will prefer one with skewAngleY == 0
+        assert not decomposedTransformEqual(decomposed, dec)
+    else:
+        assert decomposedTransformEqual(decomposed, dec)
+
+
+@pytest.mark.parametrize("decomposed, composed", test_data)
+def test_compose_decompose(decomposed, composed):
+    t1 = composeTransform(**decomposed._asdict())
+    dec = decomposeTransform(t1)
+    t2 = composeTransform(**dec._asdict())
+    assert transformEqual(t1, t2)
+    assert transformEqual(composed, t1)
+    assert transformEqual(composed, t2)
